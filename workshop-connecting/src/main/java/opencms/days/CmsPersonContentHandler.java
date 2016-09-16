e7d76cd18e7f03605a639ed2699495962447cf22
Philipp Strudthoff <p.strudthoff@alkacon.com>
2016-09-16 14:31:15 +0200

    Add first version of content for workshop-connecting.

---
+++ workshop-connecting/src/main/java/opencms/days/CmsPersonContentHandler.java
@@ -0,0 +1,49 @@
+/*
+ * File   : $Source$
+ * Date   : $Date$
+ * Version: $Revision$
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (C) 2002 - 2009 Alkacon Software (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package opencms.days;
+
+
+@SuppressWarnings("javadoc")
+public class CmsPersonContentHandler extends CmsDefaultXmlContentHandler {
+
+    /**
+     * @see org.opencms.xml.content.CmsDefaultXmlContentHandler#prepareForWrite(org.opencms.file.CmsObject, org.opencms.xml.content.CmsXmlContent, org.opencms.file.CmsFile)
+     */
+    @Override
+    public CmsFile prepareForWrite(CmsObject cms, CmsXmlContent content, CmsFile file) throws CmsException {
+
+        super.prepareForWrite(cms, content, file);
+
+        return file;
+    }
+
+}
